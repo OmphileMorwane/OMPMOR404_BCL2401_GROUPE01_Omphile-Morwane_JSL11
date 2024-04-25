a57695d80be99e7fa9f225730d49a517a14dca4e
Omphile Morwane <omphilemorwane@gmail.com>
2024-04-25 10:58:55 +0200

    Called the initilization function

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ import { initialData } from "./initialData.js";
 
 // Function checks if local storage already has data, if not it loads initialData to localStorage
 function initializeData() {
-  if (!localStorage.getItem('tasks')) {
-    localStorage.setItem('tasks', JSON.stringify(initialData)); 
-    localStorage.setItem('showSideBar', 'true')
+  if (!localStorage.getItem("tasks")) {
+    localStorage.setItem("tasks", JSON.stringify(initialData));
+    localStorage.setItem("showSideBar", "true");
   } else {
-    console.log('Data already exists in localStorage');
+    console.log("Data already exists in localStorage");
   }
 }
+initializeData();
 
 // TASK: Get elements from the DOM
 const elements = {
